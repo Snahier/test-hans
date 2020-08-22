d1b458c117d3df9122d07cb9c8a73e019aeac10f
Snahier <hansalmeida@hotmail.com>
2020-08-22 20:30:37 -0300

    Add header layout

--- web/src/pages/ProductsList/index.tsx
+++ web/src/pages/ProductsList/index.tsx
@@ -1,12 +1,31 @@
 import React from "react"
-import { ProductsListStyledContainer } from "./styles"
+import {
+  ProductsListStyledContainer,
+  Header,
+  AddProductIcon,
+  SearchWrapper,
+  AddProductButton,
+} from "./styles"
+import { Button, TextField } from "@material-ui/core"
+import { Pagination } from "@material-ui/lab"
 
 interface ProductsListProps {}
 
 const ProductsList: React.FC<ProductsListProps> = () => {
   return (
     <ProductsListStyledContainer>
-      <h1>Product list</h1>
+      <Header>
+        <h1>Product list</h1>
+
+        <AddProductButton variant="contained" color="primary">
+          <AddProductIcon /> Add a product
+        </AddProductButton>
+
+        <SearchWrapper>
+          <TextField label="Search" variant="outlined" size="small" />
+          <Pagination count={10} color="primary" />
+        </SearchWrapper>
+      </Header>
     </ProductsListStyledContainer>
   )
 }
--- web/src/pages/ProductsList/styles.ts
+++ web/src/pages/ProductsList/styles.ts
@@ -1,3 +1,43 @@
 import styled from "styled-components"
+import { Add } from "@styled-icons/material"
+import { Button } from "@material-ui/core"
 
-export const ProductsListStyledContainer = styled.section``
+export const ProductsListStyledContainer = styled.section`
+  height: 100%;
+
+  display: grid;
+  grid:
+    "header" max-content
+    "content" 0.7fr
+    /0.5fr;
+  justify-content: center;
+  align-content: center;
+`
+
+export const Header = styled.header`
+  grid-area: header;
+
+  display: grid;
+  grid-auto-rows: max-content;
+  gap: 1.5rem;
+
+  h1 {
+    margin: 0;
+  }
+`
+
+export const AddProductButton = styled(Button)`
+  width: max-content;
+`
+export const AddProductIcon = styled(Add)`
+  color: inherit;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-right: 0.5rem;
+`
+
+export const SearchWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
