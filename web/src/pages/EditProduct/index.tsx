4414de3e8307f0ff817308125fd2f3d8cd6be28c
Snahier <hansalmeida@hotmail.com>
2020-08-22 21:22:15 -0300

    Add text editor

--- web/src/pages/EditProduct/index.tsx
+++ web/src/pages/EditProduct/index.tsx
@@ -1,7 +1,9 @@
 import React from "react"
-import { EditProductStyledContainer } from "./styles"
+import { EditProductStyledContainer, EditorWrapper } from "./styles"
 import { DropzoneArea } from "material-ui-dropzone"
 import { convertMbToBytes } from "../../utils/convertMbToBytes"
+import { TextField } from "@material-ui/core"
+import MUIRichTextEditor from "mui-rte"
 
 interface EditProductProps {}
 
@@ -9,13 +11,36 @@ const EditProduct: React.FC<EditProductProps> = () => {
   return (
     <EditProductStyledContainer>
       <h1>Example name 1</h1>
-      <DropzoneArea
-        acceptedFiles={["image/*"]}
-        filesLimit={3}
-        maxFileSize={convertMbToBytes(2.5)}
-        dropzoneText="Click or drag and drop one or more images in this area"
-        showAlerts={["error", "info"]}
-      />
+
+      <main>
+        <DropzoneArea
+          acceptedFiles={["image/*"]}
+          filesLimit={3}
+          maxFileSize={convertMbToBytes(2.5)}
+          dropzoneText="Click or drag and drop one or more images in this area"
+          showAlerts={["error", "info"]}
+        />
+
+        <TextField label="Reference" variant="outlined" multiline rows={4} />
+
+        <EditorWrapper>
+          <MUIRichTextEditor
+            label="Click here to write a description"
+            controls={[
+              "title",
+              "bold",
+              "italic",
+              "underline",
+              "numberList",
+              "bulletList",
+              "undo",
+              "redo",
+            ]}
+          />
+        </EditorWrapper>
+      </main>
+
+      <aside></aside>
     </EditProductStyledContainer>
   )
 }
--- web/src/pages/EditProduct/styles.ts
+++ web/src/pages/EditProduct/styles.ts
@@ -5,10 +5,33 @@ export const EditProductStyledContainer = styled.section`
 
   display: grid;
   grid:
-    "header" max-content
-    "content" 0.7fr
-    / 0.5fr;
+    "header header" max-content
+    "content aside" 0.7fr
+    / 0.4fr 0.1fr;
   justify-content: center;
   align-content: center;
   gap: 1.5rem;
+
+  h1 {
+    grid-area: header;
+  }
+
+  main {
+    grid-area: content;
+
+    display: grid;
+    grid-auto-rows: max-content;
+    gap: 1.5rem;
+
+    border: 1px solid #000;
+    padding: 1.5rem;
+  }
+
+  aside {
+    grid-area: aside;
+  }
+`
+
+export const EditorWrapper = styled.div`
+  max-width: 700px;
 `
